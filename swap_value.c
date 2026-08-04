0b60d9d967f2bc499aaba9ede8b4a5da35d12d0e
shrutipandey005 <pandeyshruti126@gmail.com>
2026-08-04 21:34:43 +0530

    Added swap using call by value

---
+++ swap_value.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void swap(int a, int b) {
+    int temp = a;
+    a = b;
+    b = temp;
+
+    printf("Inside function: a = %d, b = %d\n", a, b);
+}
+
+int main() {
+    int x = 10, y = 20;
+
+    swap(x, y);
+
+    printf("In main: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
