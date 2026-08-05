73c60765fdcff09d58c39ad3287a7c1b55736ea1
shrutipandey005 <pandeyshruti126@gmail.com>
2026-08-05 20:57:55 +0530

    Added multiple students structure program

---
+++ multiple_students.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct Student {
+    int id;
+    char name[50];
+};
+
+int main() {
+    struct Student s[3];
+
+    for (int i = 0; i < 3; i++) {
+        printf("Enter ID and Name for student %d: ", i + 1);
+        scanf("%d %s", &s[i].id, s[i].name);
+    }
+
+    printf("\nStudent Details:\n");
+    for (int i = 0; i < 3; i++) {
+        printf("ID: %d, Name: %s\n", s[i].id, s[i].name);
+    }
+
+    return 0;
+}
