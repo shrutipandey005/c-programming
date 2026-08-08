7f3df92b472e014777c0bf004f516e8b1110f58a
shrutipandey005 <pandeyshruti126@gmail.com>
2026-08-08 21:19:53 +0530

    Added merge two arrays program

---
+++ merge_arrays.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a[5] = {1, 3, 5, 7, 9};
+    int b[5] = {2, 4, 6, 8, 10};
+    int c[10];
+
+    int i;
+
+    for(i = 0; i < 5; i++) {
+        c[i] = a[i];
+    }
+
+    for(i = 0; i < 5; i++) {
+        c[i + 5] = b[i];
+    }
+
+    printf("Merged array: ");
+    for(i = 0; i < 10; i++) {
+        printf("%d ", c[i]);
+    }
+
+    return 0;
+}
