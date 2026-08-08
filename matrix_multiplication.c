071ce70272a997ccd2619cee3dbde7d43f40dd65
shrutipandey005 <pandeyshruti126@gmail.com>
2026-08-08 21:22:56 +0530

    Added matrix multiplication program

---
+++ matrix_multiplication.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a[2][2] = {{1, 2}, {3, 4}};
+    int b[2][2] = {{5, 6}, {7, 8}};
+    int result[2][2] = {0};
+
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            for(int k = 0; k < 2; k++) {
+                result[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    printf("Result matrix:\n");
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
